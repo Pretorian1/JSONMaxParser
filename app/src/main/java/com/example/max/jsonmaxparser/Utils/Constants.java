7280f607050151c6b67ce22c24b5f2fdaa05947e
Maksym Koval <pretorian1@i.ua>
2017-02-23 23:27:16 +0200

    First commit )

---
+++ app/src/main/java/com/example/max/jsonmaxparser/Utils/Constants.java
@@ -0,0 +1,18 @@
+package com.example.max.jsonmaxparser.Utils;
+
+/**
+ * Created by Max on 23.02.2017.
+ */
+
+public class Constants {
+    public static final String URL = "https://s3.amazonaws.com/sherik/data.json";
+    public static final String ID = "id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String EMAIL = "email";
+    public static final String GENDER = "gender";
+    public static final String IP_ADDRESS = "ip_address";
+    public static final String IMAGE_URL = "image";
+    public static final String LAT = "lat";
+    public static final String LNG = "lng";
+}
